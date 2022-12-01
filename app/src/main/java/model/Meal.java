9a8344d1d2248beaae0c48d38383fcf5a28092d1
Anders Jonsson <aj224rj@student.lnu.se>
2022-12-01 14:50:35 +0100

    REFACTOR adding private validate-method

--- app/src/main/java/model/Meal.java
+++ app/src/main/java/model/Meal.java
@@ -4,14 +4,17 @@ public class Meal {
   private String name;
 
   public Meal (String name, int price) {
+    validateName(name);
+    this.name = name;
+  }
+
+  private void validateName(String name) {
     if (name == null) {
       throw new NullPointerException("Name-argument should not be null.");
     }
 
     if (name.trim() == "") {
-      throw new IllegalArgumentException("Name-argument should not be null.");
+      throw new IllegalArgumentException("Name-argument should not be an empty string.");
     }
-
-    this.name = name;
   }
 }
