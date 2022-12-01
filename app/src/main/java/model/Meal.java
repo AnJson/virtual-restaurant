ff354a70931768377b345eee530584b5eff9086e
Anders Jonsson <aj224rj@student.lnu.se>
2022-12-01 14:42:38 +0100

    REFACTOR setting a name field

--- app/src/main/java/model/Meal.java
+++ app/src/main/java/model/Meal.java
@@ -1,9 +1,13 @@
 package model;
 
 public class Meal {
+  private String name;
+
   public Meal (String name, int price) {
     if (name == null) {
       throw new NullPointerException("Name-argument should not be null.");
     }
+
+    this.name = name;
   }
 }
